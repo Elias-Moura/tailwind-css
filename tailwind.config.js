8aa252ca89406da282f846d7d05eb1e6d9443923
Elias Moura <elias.smourayt@gmail.com>
2023-11-09 11:39:29 -0300

    contruindo a newslatter.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ module.exports = {
       colors: {
         blue: {
           ...colors.blue,
+          claro: '#C5DFFF',
+          escuro: '#061E3C',
+          hover: '#1057B0'
         },
         purple: {
           ...colors.purple,
         },
+        fontFamily: {
+          inter: ['Inter', 'sans-serif']
+        }
       },
     },
     plugins: [],
